c98d30013d94608c285c366cead51b6ca3debfbc
Barry Moran <barmoran5@gmail.com>
2016-11-29 16:14:16 +0000

    email edit

--- rental/views.py
+++ rental/views.py
@@ -11,24 +11,24 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 
-# def rd_email(user, product):
-#     prod = get_object_or_404(Product, pk=5)
-#     str = render_to_string("email.html", {'user': user, 'product': product})
-#     me = "barmoran5@gmail.com"
-#     you = "barmoran5@gmail.com"
-#     msg = MIMEMultipart('alternative')
-#     msg['Subject'] = "Clubs4Hire-Your Invoice"
-#     msg['From'] = me
-#     msg['To'] = "barmoran5@gmail.com"
-#     part1 = str
-#     msg.attach(part1)
-#     s = smtplib.SMTP('smtp.gmail.com', 587)
-#     s.ehlo()
-#     s.starttls()
-#     s.ehlo()
-#     s.login('barmoran5@gmail.com', 'mafiaethos')
-#     s.sendmail(me, you, msg)
-#     return s.quit()
+def rd_email(user, product):
+    product = get_object_or_404(Product, pk=5)
+    str = render_to_string("email.html", {'user': user, 'product': product})
+    me = "barmoran5@gmail.com"
+    you = "barmoran5@gmail.com"
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = "Clubs4Hire-Your Invoice"
+    msg['From'] = me
+    msg['To'] = "barmoran5@gmail.com"
+    part1 = str
+    msg.attach(part1)
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.ehlo()
+    s.starttls()
+    s.ehlo()
+    s.login('barmoran5@gmail.com', 'mafiaethos')
+    s.sendmail(me, you, msg)
+    return s.quit()
 #
 #
 # def html_mail(request, id):
@@ -55,7 +55,7 @@ from django.shortcuts import render, get_object_or_404
 @csrf_exempt
 def paypal_return(request):
     args = {'post': request.POST, 'get': request.GET}
-    # rd_email(request.user, 5)
+    rd_email(request.user, 5)
     return render(request, 'paypal_return.html', args)
 
 
