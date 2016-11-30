b429ca30839abb0213bb9f3bde5be74e64d2544c
Barry Moran <barmoran5@gmail.com>
2016-11-30 13:58:04 +0000

    cors

--- custom_storages.py
+++ custom_storages.py
@@ -6,5 +6,8 @@ from storages.backends.s3boto import S3BotoStorage
 class StaticStorage(S3BotoStorage):
     location = settings.STATICFILES_LOCATION
 
+
 class MediaStorage(S3BotoStorage):
     location = settings.MEDIAFILES_LOCATION
+
+
